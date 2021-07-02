641a5a3d3e39b14ce538fc18444fc9c7a317bc26
Jimmyluluu <luu21@kimo.com>
2021-07-02 16:14:36 +0800

    Initial Commit

---
+++ SeaTurtleSensei/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeaTurtleSensei
+//
+//  Created by 魯敬元 on 2021/7/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeaTurtleSensei/SeaTurtleSenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaTurtleSenseiApp.swift
+//  SeaTurtleSensei
+//
+//  Created by 魯敬元 on 2021/7/2.
+//
+
+import SwiftUI
+
+@main
+struct SeaTurtleSenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

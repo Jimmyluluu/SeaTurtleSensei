6782394c50499a6238009b9ae3759b2b04e50677
Jimmyluluu <luu21@kimo.com>
2021-07-05 16:16:17 +0800

    fix

--- SeaTurtleSensei/StartView.swift
+++ SeaTurtleSensei/StartView.swift
@@ -7,14 +7,24 @@
 
 import SwiftUI
 
+
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        
+        ZStack{
+            Color(hex:"EAF9FF")
+        }.edgesIgnoringSafeArea(.all)
+        
     }
 }
 
+
+
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        Group {
+            SwiftUIView()
+        }
     }
 }

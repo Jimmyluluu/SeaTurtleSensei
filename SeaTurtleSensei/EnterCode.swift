d36ead03860ebd55ed35a515d1ca1721efcfd633
Jimmyluluu <luu21@kimo.com>
2021-07-10 14:57:45 +0800

    Step2未完成

---
+++ SeaTurtleSensei/EnterCode.swift
@@ -0,0 +1,20 @@
+//
+//  EnterCode.swift
+//  SeaTurtleSensei
+//
+//  Created by 魯敬元 on 2021/7/10.
+//
+
+import SwiftUI
+
+struct EnterCode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EnterCode_Previews: PreviewProvider {
+    static var previews: some View {
+        EnterCode()
+    }
+}
---
+++ SeaTurtleSensei/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  SeaTurtleSensei
+//
+//  Created by 魯敬元 on 2021/7/10.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}

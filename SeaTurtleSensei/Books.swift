dbbfa9d0e422b44a2fd37057c1e9e477c5426ca9
Jimmyluluu <luu21@kimo.com>
2021-07-15 21:16:02 +0800

    後端剛開始

---
+++ SeaTurtleSensei/Books.swift
@@ -0,0 +1,14 @@
+//
+//  Books.swift
+//  SeaTurtleSensei
+//
+//  Created by 魯敬元 on 2021/7/15.
+//
+
+import Foundation
+
+class Api:  ObservableObject{
+    func UrlRequest() {
+        
+    }
+}

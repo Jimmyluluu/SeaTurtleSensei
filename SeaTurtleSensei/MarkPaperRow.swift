88bdc45a392aa5f8054b5fdda1da5903dc85d313
Jimmyluluu <luu21@kimo.com>
2021-07-21 00:44:29 +0800

    改考卷

---
+++ SeaTurtleSensei/MarkPaperRow.swift
@@ -0,0 +1,12 @@
+//
+//  MarkPaperRow.swift
+//  SeaTurtleSensei
+//
+//  Created by 魯敬元 on 2021/7/20.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+
---
+++ SeaTurtleSensei/MarkPapers.swift
@@ -0,0 +1,27 @@
+//
+//  MarkPapers.swift
+//  SeaTurtleSensei
+//
+//  Created by 魯敬元 on 2021/7/20.
+//
+import Foundation
+import SwiftUI
+
+
+struct MarkPapers: View {
+    var body: some View {
+        
+        ZStack{
+            Color(hex:"EAF9FF").edgesIgnoringSafeArea(.all)
+            List {
+                Text("123")
+            }
+        }
+    }
+}
+
+struct MarkPapers_Previews: PreviewProvider {
+    static var previews: some View {
+        MarkPapers()
+    }
+}

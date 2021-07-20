69c581964ac94b7a8f73ef7f9e96da6b397b002c
Jimmyluluu <luu21@kimo.com>
2021-07-20 22:49:30 +0800

    fix

--- SeaTurtleSensei/Books.swift
+++ SeaTurtleSensei/Books.swift
@@ -33,4 +33,7 @@ class Api : ObservableObject{
         }.resume()
         
     }
+    func Connect() {
+        guard let url = URL(string: "http://192.168.50.178:3000") else { return }
+    }
 }
